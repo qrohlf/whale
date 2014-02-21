a4c4ee062f81a016426bd580cfbcceda0440befc
Quinn Rohlf <qr@qrohlf.com>
2014-02-21 12:39:23 -0800

    rename to rb so that irb doesn't throw a fit

--- manatee.rb
+++ manatee.rb
@@ -11,7 +11,7 @@ program :version, '0.0.1'
 program :description, 'network control system'
 
 
-targets = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+targets = [2, 3, 4, 5, 6, 7, 9, 10, 11, 12];
 totalframes = 72
 
 
@@ -33,7 +33,8 @@ command :clean do |c|
   c.summary = 'clean the results dir'
   c.description = ''
   c.action do |args, options|
-    puts `rm -f results/*`
+    `rm -f results/*`
+    puts "deleted stale results"
   end
 end
 
@@ -41,6 +42,10 @@ command :install do |c|
   c.syntax = 'manatee install --mkdir -n numberofmachines -f framelimit'
   c.summary = 'distributed rendering'
   c.description = ''
+  c.option '--mkdir', 'create install directory'
+  c.option '--upload', 'upload the installer'
+  c.option '--compile', 'unzip and compile the installer'
+  
   c.action do |args, options|
 
     puts "packaging installer"
@@ -74,52 +79,87 @@ command :render do |c|
   c.syntax = 'manatee render'
   c.summary = 'distributed rendering'
   c.description = ''
+  c.option '--transfer', 'download the results'
 
   c.action do |args, options|
+    `rm -f results/*`
+    puts "deleted stale results"
+
     puts "rendering files"
     threads = Array.new
     targets.each_with_index do |i, index| #todo: THREEEEAAAAAADS
       threads << Thread.new(i, index) do |i, index|
-        puts "starting thread #{i}"
-        startframe = (totalframes/targets.count+1)*index
-        endframe = startframe+(totalframes/targets.count+1)
-        endframe = totalframes if endframe > totalframes
+        print "starting thread #{index}\n"
         target = MACHINES[i];
-        # sleep 10
-        begin 
-          Net::SSH.start(target[:host], target[:user], :password => target[:pass], :timeout => 3) do |ssh|
-            print "beginning rendering of frames #{startframe} to #{endframe} on #{target[:host]}\n"
-            ssh.exec!("Xvfb :30 -ac -screen 0 1024x768x24")
-            
-            (startframe..endframe).each do |j|
-              puts "rendering frame #{j}"
-              puts ssh.exec!("cd #{INSTALL_LOCATION}/install && DISPLAY=':30' bin/lab2.1 lab21-mov #{j}") # currently segfaulting for reasons unknown
-              # now scp the file to localhost
-              result = "#{INSTALL_LOCATION}/install/lab21-mov#{sprintf("%04d", j)}.xwd"
-              puts "downloading #{result}... from #{target[:host]}"
-              Timeout::timeout(15) do 
-                Net::SCP.download!(target[:host], target[:user], result, "./results/", :ssh => { :password => target[:pass] })
-              end
-            end
-
-          end
-        rescue Timeout::Error
-          puts "Connection timed out on #{target[:host]}"
-        rescue Net::SCP::Error
-          puts "SCP Error on #{target[:host]}"
-        rescue Net::SSH::AuthenticationFailed
-          puts "Authentication failed on #{target[:host]}"
-        rescue IOError
-          puts "SSH session closed by target"
-        rescue Errno::ECONNREFUSED
-          puts "SSH connection refused by target"
-        end
+        startframe = (totalframes/targets.count+1)*index
+        endframe = startframe+(totalframes/targets.count+1)-1
+        endframe = totalframes if endframe > totalframes
+        render(startframe..endframe, target)
+        transfer(startframe..endframe, target) if options.transfer
+        
+        print "exiting thread #{index}\n"
       end
-      #ssh.loop # wait for all frames in job to finish rendering & downloading
     end
+    # wait for all threads to finish
     threads.each(&:join)
     puts "Rendering Complete"
   end
+end
+
+command :transfer do |c|
+  c.syntax = 'manatee transfer'
+  c.summary = 'distributed rendering'
+  c.description = ''
+
+  c.action do |args, options|
+    `rm -f results/*`
+    puts "deleted stale results"
+
+    puts "transferring files"
+    threads = Array.new
+    targets.each_with_index do |i, index| #todo: THREEEEAAAAAADS
+      threads << Thread.new(i, index) do |i, index|
+        print "starting thread #{index}\n"
+        target = MACHINES[i];
+        startframe = (totalframes/targets.count+1)*index
+        endframe = startframe+(totalframes/targets.count+1)-1
+        endframe = totalframes if endframe > totalframes
+        transfer(startframe..endframe, target)
+        
+        print "exiting thread #{index}\n"
+      end
+    end
+    # wait for all threads to finish
+    threads.each(&:join)
+    puts "Transfer Complete"
+  end
+end
+
+def render(frames, target) 
+  Net::SSH.start(target[:host], target[:user], :password => target[:pass], :timeout => 5) do |ssh|
+    #clean previous run synchronously
+    ssh.exec("cd #{INSTALL_LOCATION}/install && rm -rf ./*.xwd")
+    #start framebuffer synchronously
+    print "starting framebuffer on #{target[:host]}\n"
+    ssh.exec!("Xvfb :30 -ac -screen 0 1024x768x24")
+    frames.each do |framenum|
+      ssh.exec("cd #{INSTALL_LOCATION}/install && DISPLAY=':30' bin/lab2.1 lab21-mov #{framenum}")
+    end
+    ssh.loop #wait for all frames to finish rendering
+  end
+end
+
+def transfer(frames, target)
+  files = frames.map { |f| "#{INSTALL_LOCATION}/install/lab21-mov#{sprintf("%04d", f)}.xwd" }
+  print("transferring frames #{frames.first} to #{frames.last} from #{target[:host]}\n")
+  transfers = Array.new
+  Net::SCP.start(target[:host], target[:user], :password => target[:pass]) do |scp|
+    files.each do |file|
+      transfers << scp.download(file, "./results/")
+    end
+  end
+  transfers.each{|t| t.wait}
+  print "All transfers completed on #{target[:host]}\n"
 end
 
 
--- playmovie.sh
+++ playmovie.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #usage ./playmovie startframe endframe
-cd /install && make movieplayer
-cd ../results && printf "lab21-mov\n0\n$(frames)\n" | ../install/bin/movieplayer
+cd install && make movieplayer
+cd ../results && printf "lab21-mov\n$1\n$2\n" | ../install/bin/movieplayer
